f9b58a41a78c80515b172cefa18ac7d2dd076d41
James E. Jones <james.jones@puppetlabs.com>
2015-02-17 08:19:58 -0600

    remove need for xml template for powershell

--- manifests/attachdb.pp
+++ manifests/attachdb.pp
@@ -1,8 +1,8 @@
 # This defined type is to attach a zip file containing
 # mdf & ldf files into a new database within MS SQL Server 2012.
 define tse_sqlserver::attachdb (
-  $mdf_file      = 'AdventureWorks2012_Data',
-  $ldf_file      = 'AdventureWorks2012_log',
+  $mdf_file      = 'AdventureWorks2012_Data.mdf',
+  $ldf_file      = 'AdventureWorks2012_log.ldf',
   $zip_file      = 'AdventureWorks2012_Data.zip',
   $path          = 'C:\Program Files\Microsoft SQL Server\MSSQL12.MYINSTANCE\MSSQL\DATA',
   $file_source   = 'http://master.inf.puppetlabs.demo',
@@ -14,11 +14,6 @@ define tse_sqlserver::attachdb (
     target  => $path,
     creates => "${path}/${mdf_file}.mdf",
     source  => "${file_source}/${zip_file}",
-  }
-  file { "C:/AttachDatabaseConfig_${title}.xml":
-    ensure  => present,
-    content => template("${module_name}/AttachDatabaseConfig.xml.erb"),
-    require => Staging::Deploy["${title}_${zip_file}"],
   }
   exec { "Attach ${mdf_file}_${title}":
     command     => template("${module_name}/AttachDatabase.ps1"),
--- templates/AttachDatabase.ps1
+++ templates/AttachDatabase.ps1
@@ -1,35 +1,21 @@
 #
-# Load configuration XML file.
+# Attach SQL Server database
 #
-[xml]$databases = Get-Content "C:\AttachDatabaseConfig_<%= @title %>.xml"
-
-#
-# Get SQL Server database (MDF/LDF).
-#
-ForEach ($database in $databases.SQL.Databases) {
-    $mdfFilename = $database.MDF
-    $ldfFilename = $database.LDF
-    $DBName = $database.DB_Name
-Start-Sleep -s 10
-    #
-    # Attach SQL Server database
-    #
-    Add-PSSnapin SqlServerCmdletSnapin* -ErrorAction SilentlyContinue
-        If (!$?) {Import-Module "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\PowerShell\Modules\SQLPS" -WarningAction SilentlyContinue}
+Add-PSSnapin SqlServerCmdletSnapin* -ErrorAction SilentlyContinue
+If (!$?) {Import-Module "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\PowerShell\Modules\SQLPS" -WarningAction SilentlyContinue}
 If (!$?) {"Error loading Microsoft SQL Server PowerShell module. Please check if it is installed."; Exit}
+
 $attachSQLCMD = @"
 USE [master]
 GO
-CREATE DATABASE [$DBName] ON (FILENAME = '$mdfFilename.mdf'),(FILENAME = '$ldfFilename.ldf') for ATTACH
+CREATE DATABASE [<%= @title %>] ON (FILENAME = '<%= @mdf_file %>'),(FILENAME = '<%= @ldf_file %>') for ATTACH
 GO
 "@ 
     Invoke-Sqlcmd $attachSQLCMD -QueryTimeout 3600 -ServerInstance '<%= @hostname %>\<%= @db_instance %>'
 $changeowner = @"
-USE <%= title %> 
+USE <%= @title %> 
 GO
-ALTER AUTHORIZATION ON DATABASE::<%= title %> TO <%= @owner %>;
+ALTER AUTHORIZATION ON DATABASE::<%= @title %> TO <%= @owner %>;
 GO
 "@
     Invoke-Sqlcmd $changeowner -QueryTimeout 3600 -ServerInstance '<%= @hostname %>\<%= @db_instance %>'
-}
-
