0fc6d9b009d668618d6347157edf683e9424a8e8
David Roberts <david.roberts@puppetlabs.com>
2015-02-19 14:34:47 -0600

    added iso mount

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 # Main class that declares SQL, IISDB, and creates an
 # instance of the attachDB defined type.
 class tse_sqlserver {
+  include tsr_sqlserver::mount
   include tse_sqlserver::sql
   include tse_sqlserver::iisdb
   tse_sqlserver::attachdb { 'AdventureWorks2012': }
