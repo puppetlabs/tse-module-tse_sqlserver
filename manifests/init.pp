5dcd9192ce274e6898057df7442dd18829d7dbad
David Roberts <david.roberts@puppetlabs.com>
2015-02-19 16:24:59 -0600

    updated to work with puppet file server and sql server 2012

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,5 +4,5 @@ class tse_sqlserver {
   include tse_sqlserver::mount
   include tse_sqlserver::sql
   include tse_sqlserver::iisdb
-  #  tse_sqlserver::attachdb { 'AdventureWorks2012': }
+  tse_sqlserver::attachdb { 'AdventureWorks2012': }
 }
