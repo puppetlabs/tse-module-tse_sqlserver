25652fede0b325cfb2e74a063c3f3eb16bd04ec6
James E. Jones <james.jones@puppetlabs.com>
2015-12-18 13:45:41 -0600

    update formatting on init and remove unnecessary staging code

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,6 +10,7 @@ class tse_sqlserver (
   }
 
   contain tse_sqlserver::sql
+
   tse_sqlserver::attachdb { 'AdventureWorks2012':
     file_source => 'https://s3-us-west-2.amazonaws.com/tseteam/files/tse_sqlserver'
   }
