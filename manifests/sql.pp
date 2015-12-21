7f01bbacc5a568f43d35b1f8349dc95d2b2b7a61
James Jones <velocity303@gmail.com>
2015-12-21 17:06:03 -0600

    update to allow application through firewall versus port

--- manifests/sql.pp
+++ manifests/sql.pp
@@ -5,8 +5,14 @@ class tse_sqlserver::sql (
   $admin_user  = 'vagrant',
   $db_instance = 'MYINSTANCE',
   $sa_pass     = 'Password$123$',
-  $dbport      = '1438',
 ) {
+  case $::tse_sqlserver::sqlserver_version {
+    '2012':  {
+      $version_var  = 'MSSQL11'
+    }
+    '2014':  {
+      $version_var  = 'MSSQL12'
+    }
 
   reboot { 'before install':
       when => pending,
@@ -33,24 +39,33 @@ class tse_sqlserver::sql (
     admin_pass => $sa_pass,
   }
 
-  windows_firewall::exception { 'Sqlserver Access':
+  windows_firewall::exception { 'Sql browser access':
     ensure       => present,
     direction    => 'in',
     action       => 'Allow',
     enabled      => 'yes',
-    protocol     => 'Any',
-    local_port   => $dbport,
-    display_name => 'MSSQL',
+    program      => 'C:\Program Files (x86)\Microsoft SQL Server\90\Shared\sqlbrowser.exe'
+    display_name => 'MSSQL Browser',
+    description  => "MS SQL Server Browser Inbound Access, enabled by Puppet in $module_name",
+  }
+  
+  windows_firewall::exception { 'Sqlserver access':
+    ensure       => present,
+    direction    => 'in',
+    action       => 'Allow',
+    enabled      => 'yes',
+    program      => "C:\Program Files\Microsoft SQL Server\${version_var}.${db_instance}\MSSQL\Binn\sqlservr.exe"
+    display_name => 'MSSQL Access',
     description  => "MS SQL Server Inbound Access, enabled by Puppet in $module_name",
   }
 
-  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL12.MYINSTANCE\MSSQLServer\SuperSocketNetLib\Tcp\IPAll\TcpDynamicPorts':
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\${version_var}.${db_instance}\MSSQLServer\SuperSocketNetLib\Tcp\IPAll\TcpDynamicPorts':
     ensure => present,
     type   => string,
     data   => '',
   }
 
-  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL12.MYINSTANCE\MSSQLServer\SuperSocketNetLib\Tcp\IPAll\TcpPort':
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\${version_var}.${db_instance}\MSSQLServer\SuperSocketNetLib\Tcp\IPAll\TcpPort':
     ensure => present,
     type   => string,
     data   => $dbport,
